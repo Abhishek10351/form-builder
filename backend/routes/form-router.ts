95afb3b8e98000553462951fec17f4970293085b
Abhishek Kashyap <abhik5567@gmail.com>
2024-12-12 22:29:44 +0530

    feat:  add missing route

--- backend/routes/form-router.ts
+++ backend/routes/form-router.ts
@@ -1,28 +1,37 @@
 import { Request, Response, Router } from "express";
-
 import {
     createForm,
     getForm,
     listForms,
     submitForm,
+    viewSubmissions,
 } from "../controllers/form";
 
 const router = Router();
 
+// Endpoint to create a new form
 router.post("", (req: Request, res: Response) => {
     createForm(req, res);
 });
 
+// Endpoint to submit a form
 router.put("/:formId", (req: Request, res: Response) => {
     submitForm(req, res);
 });
 
+// Endpoint to list all forms
 router.get("/", (req: Request, res: Response) => {
     listForms(req, res);
 });
 
+// Endpoint to get a specific form by ID
 router.get("/:formId", (req: Request, res: Response) => {
     getForm(req, res);
+});
+
+// Endpoint to view submissions for a specific form
+router.get("/:formId/submissions", (req: Request, res: Response) => {
+    viewSubmissions(req, res);
 });
 
 export default router;
